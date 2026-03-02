6471812218d58e679f47a4ac3ae63d8d828b979c
ssucommu <bbengcanthr@gmail.com>
2026-03-02 10:50:56 +0700

    Remove unused setImage function and event listener
    Removed setImage function and click event listener for background music.

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function setImage(layer, src) {
-  document.getElementById("layer" + layer).src = src;
-}
 
-/* บาง browser ต้องคลิกก่อนเพลงจะดัง */
-window.addEventListener("click", function() {
-  document.getElementById("bgm").play();
-});
