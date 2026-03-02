a46fd0a27593274bb0e5a167ecb8dacc751bbf7a
ssucommu <bbengcanthr@gmail.com>
2026-03-02 10:12:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function setImage(layer, src) {
-  document.getElementById("layer" + layer).src = src;
+function setImage(layer, path) {
+  document.getElementById("layer" + layer).src = path;
 }
+
+/* แก้ autoplay บาง browser */
+window.addEventListener("click", function(){
+  const bgm = document.getElementById("bgm");
+  bgm.play();
+});
